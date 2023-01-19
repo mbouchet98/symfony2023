cb0d807a28dcb79326c511240d201c301ddabde5
mbouchet98 <maelysbouchet98@gmail.com>
2023-01-19 17:34:18 +0100

    affichage de la liste de personne, Ajout/Modification en cour de développement

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,32 +2,47 @@
 
 namespace App\Controller;
 
+use App\Controller\Form\FormPersonne;
 use App\Entity\Personne;
+use App\Repository\PersonneRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 
 class PersonneController extends AbstractController
 {
 
-    /**
-     * @Route("/personne", name="app_personne")
-     */
-    public function index(): Response
+    public function formAction(Request $request)
     {
+        $form = $this->createForm(FormPersonne::class);
+        $form->handleRequest($request);
 
-        $person1 = new Personne("test","test2");
-        $person2 = new Personne("test","test2");
-        $person3 = new Personne("test","test2");
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $this->get('session')->set('data', $data);
+            return $this->redirectToRoute('index.html.twig');
+        }
 
-        $listePersonnes = array($person1, $person2, $person3);
-
-        return $this->render('personne/index.html.twig', [
-            'personne' => 'Personne',
-            'listePersonne' => $listePersonnes,
-            ]);
-
+        return $this->render('form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
+    /**
+     * @Route("/personne", name="app_personne")
+     * chargement de la page avec la liste de personnes
+     */
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $monEnregistrement = $doctrine->getRepository(Personne::class)->findAll();
 
+        return $this->render('personne/index.html.twig', array('personnes'=>$monEnregistrement));
+    }
 }
